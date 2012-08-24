1302021cecb6e41c99410c7cf0f23685f9639a78
ArkeologeN <hamza@plumtreegroup.net>
2012-08-24 21:52:07 +0500

    VO,Controller and View created for the application

--- js/app.js
+++ js/app.js
@@ -1,7 +1,44 @@
-var App = Em.Application.create();
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
+/**
+ * Taking Application Instance and Initializing namespace
+ */
+var Embedos = Em.Application.create({});
+
+Embedos.TitleTextField = Em.TextField.extend({
+    insertNewline: function() {
+        //console.log(this);
+        Embedos.TodosC.loadTodos();
+    }
+});
+
+/**
+ * A simple view named ListTodoView
+ */
+Embedos.ListTodoView = Em.View.extend({
+  loaderBinding: "Embedos.TodoC.loader",
+  firstRunBinding: "Embedos.TodoC.firstRun"
+});
+
+/**
+ * Todo Model - Value Object
+ */
+Embedos.TodoVO = Em.Object.extend({
+	title: null,
+	desc: null,
+	done: false
+});
+
+
+Embedos.TodosC = Em.ArrayController.create({
+  content: [Embedos.TodoVO.create({
+          title: "Doing Ember"
+  })],
+  title: '',
+  loader: false,
+  firstRun: false,
+  loadTodos: function() {
+      var me = this;
+      var title = me.get('term');
+      console.log(this);
   }
 });
